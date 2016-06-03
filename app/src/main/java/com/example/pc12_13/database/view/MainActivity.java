dfd0eea364095bc64ac8880d59fd25a55a6b4388
PC17-04 <PC17-04>
2016-06-03 18:40:16 +0700

    First

--- app/src/main/java/com/example/pc12_13/database/view/MainActivity.java
+++ app/src/main/java/com/example/pc12_13/database/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.pc12_13.database;
+package com.example.pc12_13.database.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.pc12_13.database.R;
 
 public class MainActivity extends AppCompatActivity {
 
